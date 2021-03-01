9f9ef1a5800e7c02c752e3ea53ea5c615c767086
Svitlana <rsv0011@gmail.com>
2021-03-01 16:02:56 +0100

    create carDetails page

---
+++ src/pages/CarDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CarContext } from '../contexts/CarContext';
+
+
+export default function CarDetails(){
+  const { cars } = useContext(CarContext);
+
+  return(
+    <>
+    </>
+  )
+}
