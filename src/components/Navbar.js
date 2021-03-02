d8f3b77019b235c13ac3a10e4291557e55d61d96
Elena <sagva2014@gmail.com>
2021-03-02 16:51:57 +0100

    Added styling for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,19 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { faCar } from '@fortawesome/free-solid-svg-icons'
+import styles from '../styles/Navbar.module.css'
 
 
 const Navbar = () =>{
   return(
-    <div>
-        <h1>Logo <FontAwesomeIcon icon={faCar}/></h1>
-      <nav>
+    <div className='d-flex justify-content-between align-items-center'>
+      {/* <h1>Logo <FontAwesomeIcon icon={faCar}/></h1> */}
+      <div className='d-flex align-items-end ms-3'>
+        <img src={`../logo.png`} alt="Logo" className={styles.logo}/>
+        <div className={`${styles.brandName} ms-3`}>Car Market <p className={styles.subBrand}>Lavender</p></div>
+        
+      </div>
+      <nav className='nav'>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to='/shopping-cart'><FontAwesomeIcon icon={faShoppingCart}/></Link>
