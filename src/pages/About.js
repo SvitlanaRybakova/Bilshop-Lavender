b1d6bc50dca523d98760b5a0b0a2b828c1bdb002
Henrik <henrik.lood.mail@gmail.com>
2021-03-02 15:27:24 +0100

    Added content to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,29 +3,29 @@ import styles from "../styles/About.Module.css"
 function About(){
   return(
     <div className={`${styles.about}`}>
-      <div className="card mb-3 d-flex">
+      <div className="mb-3 d-flex">
         <div className="row g-0">
           <div className={`col-md-4 order-md-2`}>
-            <img src="../assets/car-pictures/Buick-Reatta-1989.jpg" alt="..."  className={`${styles.imgContainer}`}/>
+            <img src="../assets/car-pictures/Buick-Reatta-1989.jpg" alt="USP img 1" className={`${styles.imgContainer}`}/>
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title text-center">About Company</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 className="card-title text-sm-center text-md-start">About Company</h5>
+              <p className="card-text">Once the pickup braked the upfit. The aluminum engine was crashed by the Hino? The heavy duty, lifted pickup truck deconstructed. Once the wheel deconstructed the welder body while the upfitted hybrid was dumped by the axle.</p>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="card mb-3 d-flex">
+      <div className="mb-3 d-flex">
         <div className="row g-0">
           <div className="col-md-4">
-            <img src="../assets/car-pictures/Ford-Mustang-1969.jpg" alt="..." style={{maxWidth: "100%"}}/>
+            <img src="../assets/car-pictures/Ford-Mustang-1969.jpg" alt="USP img 2" className={`${styles.imgContainer}`}/>
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title text-center">We do things</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 className="card-title text-sm-center text-md-start">We do things</h5>
+              <p className="card-text">The axle crashed the work-ready service utility van. The aluminum axle was manufactured by the mobility. Once the rollback body demolished the lorry. Once the Hino upfitted the chipper body. The hauler body upfitted the diesel Nissan.</p>
             </div>
           </div>
         </div>
@@ -34,7 +34,7 @@ function About(){
       <div className="card text-center">
         <div className="card-body">
           <p className="card-text">We have the car just for you!</p>
-          <a href="#" className="btn btn-primary">Find your new car</a>
+          <a href="#" className="btn btnWhite">Find your new car</a>
         </div>
       </div>
     </div>
