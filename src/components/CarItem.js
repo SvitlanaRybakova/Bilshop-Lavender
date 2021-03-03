23ea7d80ded50bcc9b0832c5f1cad1bfee6eebae
Blerta <blertatoska30@gmail.com>
2021-03-03 10:18:29 +0100

    Auto stash before revert of "Merge remote-tracking branch 'origin/dev' into dev"

--- src/components/CarItem.js
+++ src/components/CarItem.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import styles from "../styles/Home.Module.css";
 
 export default function CarItem(props){
   const { car } = props;
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/cars/${car.vin}`);
+  }
   
   return(
-    <>
-    <div className="col-md-3">
-    <h6 style={{width: '200px', height:'100px', border: '1px solid green' }}>{ car.make }</h6>
+    <div className="card col-md-3">
+      <h6>{ car.make }</h6>
+      <div className={styles.imgContainer}>
+        <img className={styles.imgCars} src={`../assets/car-pictures/${car.make}-${car.model}-${car.year}.jpg`}></img>
+      </div>
+    <div 
+    onClick={handleClick}
+    className="btn"
+    >See more</div>
     </div>
-    </>
   )
 }
