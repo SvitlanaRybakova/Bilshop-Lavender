a8c4c6ed606e98101d43ae4166a6ab12ee469a82
Svitlana <rsv0011@gmail.com>
2021-03-04 13:07:06 +0100

    added useStates and main variables for pagenation

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,17 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CarContext } from "../contexts/CarContext";
 import CarItem from "../components/CarItem";
+import PagePagination from '../components/PagePagination'
 
 export default function CarList() {
-  const { cars } = useContext(CarContext);
+  const  {cars}  = useContext(CarContext);
+
+  // pagination
+  
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(9)
+
+  // временный массив, ограничивающий количество постов
+  const [temp, setTemp] = useState([]);
+
+  useEffect(() => {
+    setTemp(cars);
+    console.log(temp);
+  },[])
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = temp.slice(indexOfFirstPost, indexOfLastPost);
 
   return (
     <div className="container container-wide">
       <div className="row mtn-30 d-flex justify-content-center">
-        {cars.map((item) => (
-          <CarItem key={item.vin} car={item} />
+        {currentPosts.map((item) => (
+          <CarItem key={item.vin} car={cars} />
         ))}
       </div>
+      <PagePagination/>
     </div>
   );
 }
