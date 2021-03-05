03e43eb2879a454eaa734b6659632026860b7f9e
Elena <sagva2014@gmail.com>
2021-03-05 18:49:31 +0100

    Resolving merge conflict

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,20 +19,20 @@ const Navbar = () => {
       <div className="container d-flex justify-content-end">
         <div className='d-flex align-items-end flex-grow-1'>
           <Link to="/" className='d-flex align-items-end'>
-            <img src={`../../logo.png`} alt="Logo" className='logo' />
+            <img src={`../../logo.png`} alt="Logo" className='logo'/>
             <div className='brandName ms-3'>Car Market <p className='subBrand'>Lavender</p></div>
           </Link>
-          <button>
+          <button 
             onClick={toggleMenu}
-            style={{ border: 'none' }}
+            style={{border: 'none'}} 
             className={`${styles.myBtn} btn navbar-toggler`}
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" 
+            aria-expanded="false" 
             aria-label="Toggle navigation">
-            <FontAwesomeIcon icon={faBars} />
+            <FontAwesomeIcon icon={faBars}/>
           </button>
           <div className={`${show} ${styles.navbarList} collapse navbar-collapse`} id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-lg-0 d-flex align-items-end">
@@ -44,7 +44,7 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <Link className={styles.shoppingCartIcon} to='/shopping-cart'><FontAwesomeIcon icon={faShoppingCart} /></Link>
+          <Link className={styles.shoppingCartIcon} to='/shopping-cart'><FontAwesomeIcon icon={faShoppingCart}/></Link>
         </div>
       </div>
     </nav>
