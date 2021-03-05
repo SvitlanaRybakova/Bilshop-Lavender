a26137f52fee9e7db9bfb75aa0a8160e6f9a22d0
Elena <sagva2014@gmail.com>
2021-03-05 18:53:27 +0100

    Resolving merge conflict2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,38 +14,39 @@ const Navbar = () => {
 
   const show = showCollapsedMenu ? "show" : "";
 
+
   return (
     <nav className="navbar navbar-expand-sm">
       <div className="container d-flex justify-content-end">
         <div className='d-flex align-items-end flex-grow-1'>
           <Link to="/" className='d-flex align-items-end'>
-            <img src={`../../logo.png`} alt="Logo" className='logo'/>
-            <div className='brandName ms-3'>Car Market <p className='subBrand'>Lavender</p></div>
+            <img src={`../logo.png`} alt="Logo" className='logo' />
+            <div className={`${styles.brandName} ms-3`}>Car Market <p className='subBrand'>Lavender</p></div>
           </Link>
-          <button 
-            onClick={toggleMenu}
-            style={{border: 'none'}} 
-            className={`${styles.myBtn} btn navbar-toggler`}
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarSupportedContent" 
-            aria-controls="navbarSupportedContent" 
-            aria-expanded="false" 
-            aria-label="Toggle navigation">
-            <FontAwesomeIcon icon={faBars}/>
-          </button>
-          <div className={`${show} ${styles.navbarList} collapse navbar-collapse`} id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-lg-0 d-flex align-items-end">
-              <li className="nav-item">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/about">About</Link>
-              </li>
-            </ul>
-          </div>
-          <Link className={styles.shoppingCartIcon} to='/shopping-cart'><FontAwesomeIcon icon={faShoppingCart}/></Link>
         </div>
+        <button
+          onClick={toggleMenu}
+          style={{ border: 'none' }}
+          className={`${styles.myBtn} btn navbar-toggler`}
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <div className={`${show} ${styles.navbarList} collapse navbar-collapse`} id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-lg-0 d-flex align-items-end">
+            <li className="nav-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+        <Link className={styles.shoppingCartIcon} to='/shopping-cart'><FontAwesomeIcon icon={faShoppingCart} /></Link>
       </div>
     </nav>
   );
