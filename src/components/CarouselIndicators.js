75d781cb879b539d68a2f7d3f82b843c4c7fcbb9
Svitlana <rsv0011@gmail.com>
2021-03-10 10:58:25 +0100

    refactored carouselIndecators

--- src/components/CarouselIndicators.js
+++ src/components/CarouselIndicators.js
@@ -1,27 +1,17 @@
 function CarouselIndicators(props) {
   const { index } = props;
 
-  if(index === 0){
+  
     return(
         <button
           type="button"
           data-bs-target="#carouselExampleDark"
           data-bs-slide-to={index}
-          className="active"
-          aria-current="true"
-          aria-label={`Slide ${index + 1}`}
-        ></button>
-    )
-  }else{
-    return(
-      <button
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide-to={index}
+          className={`${index === 0 ? "active" : "" }`}
+          aria-current={`${index === 0 ? "true" : "" }`}
           aria-label={`Slide ${index + 1}`}
         ></button>
     )
   }
-}
 
 export default CarouselIndicators;
