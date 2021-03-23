6c0cd5e57aedbb03f6374b5a82e67e9b61abbe8a
Elena <sagva2014@gmail.com>
2021-03-23 14:58:14 +0100

    Made Advanced filter collapsed by default

--- src/components/PriceRange.js
+++ src/components/PriceRange.js
@@ -29,12 +29,12 @@ function PriceRange() {
         <div className="accordion-item">
           <div className="container">
             <h2 className="accordion-header" id="headingOne">
-              <button className={`${styles.accordionBtn} accordion-button`} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              <button className={`${styles.accordionBtn} accordion-button collapsed`} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                 <span className={styles.btnText}>Advanced searching</span>
               </button>
             </h2>
           </div>
-          <div id="collapseOne" className={`${styles.accordionColapse} accordion-collapse collapse show`} aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+          <div id="collapseOne" className={`${styles.accordionColapse} accordion-collapse collapse`} aria-labelledby="headingOne" data-bs-parent="#accordionExample">
             <div className="accordion-body p-0 m-0">
               {/* body */}
               <div className={`${styles.filtercontainer} container`}>
